e03e6993068a742277b184f57c1512355d03e9cc
nuclearmistake <nuclearmistake@gmail.com>
2014-03-23 11:22:10 -0400

    Revert "Revert "Revert "Camera3: Work around for streams with different o/p rates"""
    This reverts commit 2eb35a06d1a3767ea598ecc7e58fa58fe81fd91f.

--- QCamera2/HAL3/QCamera3Channel.cpp
+++ QCamera2/HAL3/QCamera3Channel.cpp
@@ -1777,7 +1777,7 @@ QCamera3Exif *QCamera3PicChannel::getExifData()
     return exif;
 }
 
-int QCamera3PicChannel::kMaxBuffers = 2;
+int QCamera3PicChannel::kMaxBuffers = 1;
 
 /*===========================================================================
  * FUNCTION   : QCamera3ReprocessChannel
